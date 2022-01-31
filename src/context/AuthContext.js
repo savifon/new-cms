8bbfd71c9ef9476c89d14b8f10f580bfa9a7e3b7
savifon <savifon1@gmail.com>
2022-01-30 22:00:17 -0300

    integrando authcontext no approutes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -51,4 +51,4 @@ export const AuthProvider = ({ children }) => {
     );
 };
 
-export default { AuthContext };
+export { AuthContext };
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
     BrowserRouter as Router,
     Route,
     Routes,
-    // Navigate,
+    Navigate,
 } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Admin from "../pages/Admin";
 
+import { AuthProvider, AuthContext } from "../context/AuthContext";
+
 const AppRoutes = () => {
+    const Private = ({ children }) => {
+        const { authenticated, loading } = useContext(AuthContext);
+
+        if (loading) {
+            return <div>Carregando...</div>;
+        }
+
+        if (!authenticated) {
+            return <Navigate to="/login" />;
+        }
+    };
+
     return (
         <Router>
-            <Routes>
-                <Route exact path="/" element={<Admin />} />
-                <Route exact path="/login" element={<Login />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route exact path="/" element={<Admin />} />
+                    <Route
+                        exact
+                        path="/login"
+                        element={
+                            <Private>
+                                <Login />
+                            </Private>
+                        }
+                    />
+                </Routes>
+            </AuthProvider>
         </Router>
     );
 };
