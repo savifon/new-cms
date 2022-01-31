44ef84927a93ba6cb4752c4dccde8f6a124e2324
savifon <savifon1@gmail.com>
2022-01-31 02:27:09 -0300

    renderização dos módulos

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -4,8 +4,20 @@ import Header from "../../components/Header";
 import Menu from "../../components/Menu";
 import Footer from "../../components/Footer";
 
+import Users from "../../container/Users";
+
 const Admin = (props) => {
     const { module, title } = props;
+
+    const renderModule = (module) => {
+        switch (module) {
+            case "users":
+                return <Users />;
+
+            default:
+                return <p>Nenhum!!!</p>;
+        }
+    };
 
     return (
         <>
@@ -16,6 +28,8 @@ const Admin = (props) => {
                 <div className="p-4 w-[80vw]">
                     <h1>{title}</h1>
                     <p>{module}</p>
+
+                    {renderModule(module)}
                 </div>
             </div>
             <Footer />
