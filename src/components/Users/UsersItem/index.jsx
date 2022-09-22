99426312fea6c3c0db154deaf156a01e446b7580
savifon <savifon1@gmail.com>
2022-09-21 21:47:47 -0300

    feature: update user

--- src/components/Users/UsersItem/index.jsx
+++ src/components/Users/UsersItem/index.jsx
@@ -1,93 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { supabase } from "../../../api/supabaseClient";
 
 const User = (props) => {
     const { title, data } = props;
-    console.log(data.id);
+    const navigate = useNavigate();
+    const [username, setUsername] = useState(data.user_name);
+    const [fullName, setFullName] = useState(data.full_name);
+    const [email, setEmail] = useState(data.email);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (event) => {
+        let response;
+        event.preventDefault();
+        setLoading(true);
+
+        if (username && fullName && email) {
+            response = await supabase
+                .from("users")
+                .update({
+                    user_name: username,
+                    full_name: fullName,
+                    email: email,
+                })
+                .eq("id", data.id);
+        } else {
+            alert("Preencha todos os campos!");
+        }
+
+        if (response.status === 200) return navigate("/users");
+        alert("Erro ao atualizar usuário!");
+        setLoading(false);
+    };
 
     if (data) {
         return (
             <>
-                <h1>Form</h1>
                 <p>{title}</p>
 
                 <br />
 
-                <form
-                    action=""
-                    className="flex items-start flex-wrap gap-x-[10px] gap-y-[15px]"
-                >
-                    <input type="hidden" name="id" value={data.id} />
+                <form onSubmit={handleSubmit} method="post">
+                    <div className="flex items-start flex-wrap gap-x-[10px] gap-y-[15px] mb-3">
+                        <input type="hidden" name="id" value={data.id} />
 
-                    <label htmlFor="created_at" className="flex flex-col">
-                        Criado em
-                        <input
-                            disabled
-                            type="text"
-                            name="created_at"
-                            id="created_at"
-                            value={data.created_at}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400 opacity-50"
-                        />
-                    </label>
+                        <label htmlFor="created_at" className="flex flex-col">
+                            Criado em
+                            <input
+                                disabled
+                                type="text"
+                                name="created_at"
+                                id="created_at"
+                                value={data.created_at}
+                                className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400 opacity-50"
+                            />
+                        </label>
 
-                    <label htmlFor="user_name" className="flex flex-col">
-                        Nome de usuário
-                        <input
-                            type="text"
-                            name="user_name"
-                            id="user_name"
-                            value={data.user_name}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
-                        />
-                    </label>
+                        <label htmlFor="user_name" className="flex flex-col">
+                            Nome de usuário
+                            <input
+                                type="text"
+                                name="user_name"
+                                id="user_name"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
+                            />
+                        </label>
 
-                    <label htmlFor="full_name" className="flex flex-col">
-                        Nome completo
-                        <input
-                            type="text"
-                            name="full_name"
-                            id="full_name"
-                            value={data.full_name}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
-                        />
-                    </label>
+                        <label htmlFor="full_name" className="flex flex-col">
+                            Nome completo
+                            <input
+                                type="text"
+                                name="full_name"
+                                id="full_name"
+                                value={fullName}
+                                onChange={(e) => setFullName(e.target.value)}
+                                className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
+                            />
+                        </label>
 
-                    <label htmlFor="email" className="flex flex-col">
-                        E-mail
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={data.email}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
-                        />
-                    </label>
-
-                    <label htmlFor="password" className="flex flex-col">
-                        Senha
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            value={data.password}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
-                        />
-                    </label>
-
-                    <label htmlFor="id_users_group" className="flex flex-col">
-                        Grupo
-                        <input
-                            type="text"
-                            name="id_users_group"
-                            id="id_users_group"
-                            value={data.id_users_group}
-                            className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
-                        />
-                    </label>
+                        <label htmlFor="email" className="flex flex-col">
+                            E-mail
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="border-2 rounded-md p-2 w-[300px] focus:border-gray-400"
+                            />
+                        </label>
+                    </div>
 
                     <button
                         type="submit"
-                        className="bg-gray-800 text-white font-semibold p-2 rounded-md w-[300px] border-2 border-gray-900 hover:bg-gray-900 hover:border-gray-600 hover:shadow-xl transition-all"
+                        className={`
+                            ${loading ? "cursor-not-allowed" : "cursor-pointer"}
+                            bg-gray-800 text-white font-semibold p-2 rounded-md w-[300px] border-2 border-gray-900 hover:bg-gray-900 hover:border-gray-600 hover:shadow-xl transition-all`}
+                        disabled={loading}
                     >
                         Salvar
                     </button>
